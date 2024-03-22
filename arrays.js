24502396f56cc977c5034f41c5578a6cfc91039e
amukoaterry <amukoaterrymitchell@gmail>
2024-03-22 17:03:17 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,29 @@
+//find the last element of the array//
+
+let arr1 = [3,7,34,90,12];
+console.log[arr1[arr1.length]];
+
+//Finding the last element//
+let arr2 = new Array(true,"green","where","12,56");
+let sliceArray = arr2.slice(-1);
+console.log({sliceArray});
+
+
+
+
+//Joining strings//
+
+const stringmyPets = ["Cow","Bird","Snake","Dogs"];
+let x = stringmyPets.toString();
+console.log({x});
+
+
+//sort the followin array//
+const arr3 = [-5,9,5,3,2,-3,68,4,1];
+let p = arr3.sort();
+console.log({p});
+
+
+
+
+
